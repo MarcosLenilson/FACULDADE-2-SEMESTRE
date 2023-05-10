078728b673dacc564cd0bfae6ac0d7c8204bb390
Marcos Lenilson Alves Duarte <janajesuslelemarcos@gmail.com>
2023-05-10 05:48:21 -0300

    Update TELE AULA 3 - AULA 3 - FUNÇÕES DE AGREGAÇÃO EM BANCO DE DADOS.sql

--- PROGRAMAÇÃO E DESENVOLVIMENTO DE BANCO DE DADOS/TELE AULA 3 - AULA 3 - FUNÇÕES DE AGREGAÇÃO EM BANCO DE DADOS.sql
+++ PROGRAMAÇÃO E DESENVOLVIMENTO DE BANCO DE DADOS/TELE AULA 3 - AULA 3 - FUNÇÕES DE AGREGAÇÃO EM BANCO DE DADOS.sql
@@ -33,7 +33,6 @@ DESCRIBE veiculos;
 
 INSERT veiculos
 VALUES('0', 'BMW', '320i', 160000.00),
-VALUES('0', 'BMW', '320i', 160000.00),
     ('0', 'Mercedes_Bens', 'C180', 140000.00),
     ('0', 'Hyundai', 'Azera', 120000.00),
     ('0', 'Mercedes_Bens', 'CLA 200', 140000.00),
@@ -44,8 +43,62 @@ VALUES('0', 'BMW', '320i', 160000.00),
     ('0', 'Mercedes_Bens', 'C250', 180000.00),
     ('0', 'Jaguar', 'XF', 220000.00),
     ('0', 'BMW', '535i', 500000.00),
-    ('0', 'Jaguar', 'VZ');
+    ('0', 'Jaguar', 'VZ', NULL);
     
 SELECT * FROM veiculos;
-    Error Code: 1136. Column count doesn't match value count at row 12
+
+
+
+###########################>>>>>>>>>>  FUNÇÕES  <<<<<<<<<<###########################
+
+## COUNT
+	-- Esta função permite que se possa contar o numero de registros de uma relação 
+    
+SELECT COUNT(*) FROM veiculos;
+	-- Podemos observar que são demonstrados os INSERTS da tabela “Veiculos”, e podemos perceber que na coluna “Id” o auto incremento registrou 12 entradas.
+
+SELECT COUNT(Valor) FROM Veiculos;
+	-- ATENÇÃO ao utilizar o COUNT, devido contador ignorar os registros em que haja valor nulo (NULL)
+
+## DISTINCT 
+	-- A função DISTINCT, é utilizada juntamente com o COUNT
+
+SELECT COUNT( DISTINCT MARCA) FROM Veiculos; 	
+	-- Esta função separa DISTINTAMENTE os valores, não repetindo valores iguais.
+    
+## TOTAL (SUM)
+	-- A função SUM retorna o somatório dos valores em uma determinada coluna.
+
+SELECT SUM(valor) AS "TOTAL" FROM veiculos;
+	-- Retornou o valor somado da coluna "VALOR"
+
+## MINIMUM (MIN)
+	-- A função agregada MIN permite que se possa determinar o menor valor de registro em uma coluna.
+	
+SELECT MIN(valor) AS "MENOR VALOR" FROM veiculos;	
+	-- Vamos selecionar a marca, o modelo e o veículo de menor valor registrado na tabela.
+SELECT Marca, Modelo, MIN(Valor) AS "MENOR VALOR" 
+	FROM Veiculos;
+   
+## MAXIMUM (MAX)
+	-- Analogamente à função MIN, o recurso MAX permite que se possa determinar o maior valor de registro em uma coluna. 
+
+SELECT MAX(valor) FROM veiculos; 
+	-- Vamos selecionar a marca, o modelo e o veículo de maior valor registrado na tabela.
+SELECT Marca, Modelo, MAX(Valor) AS "MAIOR VALOR"
+	FROM Veiculos;
+
+
+SELECT Marca FROM veiculos;
+SELECT Modelo FROM veiculos;
+SELECT Marca, Modelo , MAX(Valor) FROM veiculos;
+
+## AVERAGE (AVG) quer dizer MÉDIA
+	-- Retorna a Média dos valores de uma determinada coluna.
+    
+SELECT AVG(Valor) AS "Valor Médio" FROM Veiculos;
+	-- O SQL efetuou o somatório somente dos veículos com valor diferente de nulo, e fez a divisão pelo número de registros com valor também diferente de nulo.
+
+SELECT AVG(Valor) AS "Valor Médio" FROM Veiculos GROUP BY Marca;	
+	-- A função de agregação AVG permite que o qualificador GROUP BY seja utilizado.
 
